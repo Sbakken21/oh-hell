0c114aeeee736468501322a413e36809af40fc3d
Skyler Bakken <sbakken21@gmail.com>
2022-04-04 21:06:03 -0500

    initial commit

---
+++ api/index.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get(
+    "/",
+    async (req: Request, res: Response): Promise<Response> => {
+        return res.status(200).send({
+            message: "Hello world!",
+        });
+    }
+);
+
+try {
+    app.listen(port, (): void => {
+        console.log(`Connected to port ${port}`);
+    });
+} catch (error: any) {
+    console.log(`Error occured: ${error.message}`);
+}
